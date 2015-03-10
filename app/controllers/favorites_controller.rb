d45fbb6f76379e4c6ae12a98cd489e52a9108df4
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-03-10 15:45:23 -0400

    small fix to posting new faves

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
   def create
-    @fav = Favorite.create(params[:favorite])
+    @fav = Favorite.create(fave_type: params[:type], fav_id: params[:id])
     @fav.update user_id: current_user.id
     @fav.save!
     render json: @fav.to_json
